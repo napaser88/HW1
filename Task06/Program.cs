5c6d3ec27576b185ea356ba2a1e488f00799a156
DenisSilaev <denisilaev88@gmail.com>
2023-07-27 00:37:12 +0300

    some changes in Task06

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -7,9 +7,9 @@ Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number % 2 == 0)
 {
-    Console.WriteLine($"{number} - чётное число");
+    Console.WriteLine($"Число {number} является чётным");
 }
 else
 {
-    Console.WriteLine($"{number} - нечётное число");
+    Console.WriteLine($"Число {number} не является чётным");
 }
