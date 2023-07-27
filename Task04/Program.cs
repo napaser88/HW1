34304b11c5c2be809fdc5a3bf83deff0d00a21ea
DenisSilaev <denisilaev88@gmail.com>
2023-07-27 14:04:58 +0300

    fixed Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine("Введите второе число:");
 int numberB = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число:");
 int numberC = Convert.ToInt32(Console.ReadLine());
-int max = 0;
+int max = numberA;
 if (numberA > max)
 {
     max = numberA;
