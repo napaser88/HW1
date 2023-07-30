33d08c3735b17ee9215f8ced7e391f447f1c06fc
DenisSilaev <denisilaev88@gmail.com>
2023-07-30 13:53:35 +0300

    Added Task13 solution

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+bool userNumberValidate(int userNumber)
+{
+    if (userNumber < 100)
+    {
+        Console.WriteLine("У введенного числа третьей цифры нет.");
+        return false;
+    }
+    return true;
+}
+
+int thirdDigit(int userNumber)
+{
+    while (userNumber > 999)
+    {
+        userNumber /= 10;
+    }
+    return userNumber % 10;
+}
+
+Console.WriteLine("Введите целое число:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumberValidate(userNumber))
+{
+    Console.WriteLine($"Третья цифра введенного числа -> {thirdDigit(userNumber)}");
+}
