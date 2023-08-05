4ae9743889509190bfdef7964f7d9bb46576fb5b
DenisSilaev <denisilaev88@gmail.com>
2023-08-05 18:16:33 +0300

    Added Task25 solution

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -2,3 +2,18 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+int NumberDegree(int a, int b){
+    int deg = 1;
+    for(int i = 1; i <= b; i++){
+        deg = a * deg;
+    }
+    return deg;
+}
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int degree = NumberDegree(num1, num2);
+Console.WriteLine($"Число {num1} в степени {num2} равно {degree}");
