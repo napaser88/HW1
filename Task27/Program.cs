8f1ec8643fc537f0f5830c18d7b0daa85cd526e6
DenisSilaev <denisilaev88@gmail.com>
2023-08-06 17:36:26 +0300

    Added Task27 solution

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -2,3 +2,19 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int SumOfDigits(int num)
+{
+    int sum = 0;
+    while(num != 0)
+    {
+        sum += num % 10;
+        num = num / 10;   
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+int summary = SumOfDigits(userNum);
+Console.WriteLine($"Сумма цифр в числе {userNum} равна {summary}");
