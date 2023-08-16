8478d17541a951f430f6e213b9295f8b6dfe1ff5
DenisSilaev <denisilaev88@gmail.com>
2023-08-16 23:43:04 +0300

    Added Task36 solution

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -2,3 +2,51 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int SumNumbersOfNotEvenPositionArray(int[] arr)
+{
+    int sumNumNotEvenPos = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i % 2 != 0) sumNumNotEvenPos += arr[i];
+    }
+    return sumNumNotEvenPos;
+}
+
+Console.WriteLine("Пожалуйста, укажите желаемое количество элементов массива: ");
+int arrSize = Convert.ToInt32(Console.ReadLine());
+if(arrSize <= 0)
+{
+    Console.WriteLine("Невозможно построить массив с указанным количеством элементов.");
+}
+else
+{
+    int[] array = CreateArray(arrSize, -100, 100);
+    PrintArray(array);
+    Console.WriteLine();
+    int sum = SumNumbersOfNotEvenPositionArray(array);
+    Console.WriteLine($"Сумма элементов массива, стоящих на нечётных позициях = {sum}");
+}
