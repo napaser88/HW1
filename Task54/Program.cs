2bd5c79eb4ef73d2c437b09d215ae8caf1fc110e
DenisSilaev <denisilaev88@gmail.com>
2023-09-10 23:08:39 +0300

    Added Task54 solution

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -8,3 +8,60 @@
 // 9 5 3 2
 // 8 4 4 2
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void DescendRowInArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int d = 0; d < arr.GetLength(1) - j - 1; d++)
+            {
+                if (arr[i, d] < arr[i, d + 1])
+                {
+                    (arr[i, d], arr[i, d + 1]) = (arr[i, d + 1], arr[i, d]);
+                }
+            }
+        }
+    }
+}
+
+Console.WriteLine("Пожалуйста, укажите количество строк массива: ");
+int userRow = Convert.ToInt32(Console.ReadLine());
+if (userRow <= 0) Console.WriteLine("Недопустимое количество строк массива");
+Console.WriteLine("Пожалуйста, укажите количество колонн массива: ");
+int userColumn = Convert.ToInt32(Console.ReadLine());
+if (userColumn <= 0) Console.WriteLine("Недопустимое количество колонн массива");
+
+int[,] array2d = CreateMatrixRndInt(userRow, userColumn, 1, 10);
+Console.WriteLine($"Двумерный массив размером {userRow}x{userColumn} ->");
+PrintMatrix(array2d);
+Console.WriteLine($"Двумерный массив размером {userRow}x{userColumn} с упорядоченными по убыванию строками ->");
+DescendRowInArray(array2d);
+PrintMatrix(array2d);
