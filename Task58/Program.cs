03914031ef767d2b0f1776b513926a662fcc24fa
DenisSilaev <denisilaev88@gmail.com>
2023-09-11 00:21:42 +0300

    Added Task58 solution

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -6,3 +6,57 @@
 // 18 20
 // 15 18
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MatrixMultiplication(int[,] matr1, int[,] matr2)
+{
+    int multi = 0;
+    int[,] arr = new int[matr1.GetLength(0), matr2.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int d = 0; d < arr.GetLength(1); d++)
+            {
+                multi += matr1[i, d] * matr2[d, j];
+            }
+            arr[i, j] = multi;
+        }
+    }
+    return arr;
+}
+
+int[,] matrix1 = CreateMatrixRndInt(2, 2, 1, 10);
+Console.WriteLine("Первая матрица ->");
+PrintMatrix(matrix1);
+int[,] matrix2 = CreateMatrixRndInt(2, 2, 1, 10);
+Console.WriteLine("Вторая матрица ->");
+PrintMatrix(matrix2);
+int[,] multiMatrix = MatrixMultiplication(matrix1, matrix2);
+Console.WriteLine("Произведение матриц ->");
+PrintMatrix(multiMatrix);
