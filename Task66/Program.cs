601a52f906f89060190a0fa91fa957feb0fffdf9
DenisSilaev <denisilaev88@gmail.com>
2023-09-14 01:51:16 +0300

    Added Task66 solution

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -2,3 +2,19 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int SumRangeNumbers(int numM, int numN)
+{
+    if (numM == numN) return numM;
+    else if (numM < numN) return numN + SumRangeNumbers(numM, numN - 1);
+    else return numN + SumRangeNumbers(numM, numN + 1);
+}
+
+Console.WriteLine("Введите первое натуральное число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+if (numberM < 0) numberM = Math.Abs(numberM);
+Console.WriteLine("Введите второе натуральное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if (numberN < 0) numberN = Math.Abs(numberN);
+
+int amount = SumRangeNumbers(numberM, numberN);
+Console.WriteLine($"Сумма чисел в промежутке от {numberM} до {numberN} равна {amount}");
